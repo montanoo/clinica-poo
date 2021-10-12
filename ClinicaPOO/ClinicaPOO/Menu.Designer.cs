2fb20f6cdba8683ecbaa3b61ba38fd7d58b30e0d
Luciana <lucianamunguia@gmail.com>
2021-10-12 14:39:44 -0600

    Menu

--- ClinicaPOO/ClinicaPOO/Menu.Designer.cs
+++ ClinicaPOO/ClinicaPOO/Menu.Designer.cs
@@ -29,12 +29,231 @@ namespace ClinicaPOO
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.barramenu = new System.Windows.Forms.PictureBox();
+            this.btn_listapp = new System.Windows.Forms.PictureBox();
+            this.btn_bookapp = new System.Windows.Forms.PictureBox();
+            this.btn_dentists = new System.Windows.Forms.PictureBox();
+            this.btn_profile = new System.Windows.Forms.PictureBox();
+            this.btn_pharmacy = new System.Windows.Forms.PictureBox();
+            this.btn_medicines = new System.Windows.Forms.PictureBox();
+            this.labelbooking = new System.Windows.Forms.Label();
+            this.labelapp = new System.Windows.Forms.Label();
+            this.labeldentist = new System.Windows.Forms.Label();
+            this.labelprofile = new System.Windows.Forms.Label();
+            this.labelpharmacy = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.barramenu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_listapp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_bookapp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_dentists)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_profile)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_pharmacy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_medicines)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // barramenu
+            // 
+            this.barramenu.BackColor = System.Drawing.Color.Transparent;
+            this.barramenu.BackgroundImage = global::ClinicaPOO.Properties.Resources.barra;
+            this.barramenu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.barramenu.Location = new System.Drawing.Point(0, -2);
+            this.barramenu.Name = "barramenu";
+            this.barramenu.Size = new System.Drawing.Size(1026, 67);
+            this.barramenu.TabIndex = 0;
+            this.barramenu.TabStop = false;
+            // 
+            // btn_listapp
+            // 
+            this.btn_listapp.BackColor = System.Drawing.Color.Transparent;
+            this.btn_listapp.BackgroundImage = global::ClinicaPOO.Properties.Resources.clipboard_1;
+            this.btn_listapp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_listapp.Location = new System.Drawing.Point(416, 283);
+            this.btn_listapp.Name = "btn_listapp";
+            this.btn_listapp.Size = new System.Drawing.Size(212, 211);
+            this.btn_listapp.TabIndex = 1;
+            this.btn_listapp.TabStop = false;
+            // 
+            // btn_bookapp
+            // 
+            this.btn_bookapp.BackColor = System.Drawing.Color.Transparent;
+            this.btn_bookapp.BackgroundImage = global::ClinicaPOO.Properties.Resources.appointment_1;
+            this.btn_bookapp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_bookapp.Location = new System.Drawing.Point(134, 312);
+            this.btn_bookapp.Name = "btn_bookapp";
+            this.btn_bookapp.Size = new System.Drawing.Size(168, 168);
+            this.btn_bookapp.TabIndex = 2;
+            this.btn_bookapp.TabStop = false;
+            // 
+            // btn_dentists
+            // 
+            this.btn_dentists.BackColor = System.Drawing.Color.Transparent;
+            this.btn_dentists.BackgroundImage = global::ClinicaPOO.Properties.Resources.dentist__1__1;
+            this.btn_dentists.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_dentists.Location = new System.Drawing.Point(753, 312);
+            this.btn_dentists.Name = "btn_dentists";
+            this.btn_dentists.Size = new System.Drawing.Size(168, 168);
+            this.btn_dentists.TabIndex = 3;
+            this.btn_dentists.TabStop = false;
+            // 
+            // btn_profile
+            // 
+            this.btn_profile.BackColor = System.Drawing.Color.Transparent;
+            this.btn_profile.BackgroundImage = global::ClinicaPOO.Properties.Resources.user__1__1;
+            this.btn_profile.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_profile.Location = new System.Drawing.Point(134, 666);
+            this.btn_profile.Name = "btn_profile";
+            this.btn_profile.Size = new System.Drawing.Size(167, 168);
+            this.btn_profile.TabIndex = 4;
+            this.btn_profile.TabStop = false;
+            // 
+            // btn_pharmacy
+            // 
+            this.btn_pharmacy.BackColor = System.Drawing.Color.Transparent;
+            this.btn_pharmacy.BackgroundImage = global::ClinicaPOO.Properties.Resources.pharmacy_1;
+            this.btn_pharmacy.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_pharmacy.Location = new System.Drawing.Point(434, 666);
+            this.btn_pharmacy.Name = "btn_pharmacy";
+            this.btn_pharmacy.Size = new System.Drawing.Size(175, 175);
+            this.btn_pharmacy.TabIndex = 5;
+            this.btn_pharmacy.TabStop = false;
+            // 
+            // btn_medicines
+            // 
+            this.btn_medicines.BackColor = System.Drawing.Color.Transparent;
+            this.btn_medicines.BackgroundImage = global::ClinicaPOO.Properties.Resources.pill_1;
+            this.btn_medicines.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_medicines.Location = new System.Drawing.Point(746, 659);
+            this.btn_medicines.Name = "btn_medicines";
+            this.btn_medicines.Size = new System.Drawing.Size(175, 175);
+            this.btn_medicines.TabIndex = 6;
+            this.btn_medicines.TabStop = false;
+            // 
+            // labelbooking
+            // 
+            this.labelbooking.AutoSize = true;
+            this.labelbooking.BackColor = System.Drawing.Color.Transparent;
+            this.labelbooking.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelbooking.ForeColor = System.Drawing.Color.White;
+            this.labelbooking.Location = new System.Drawing.Point(92, 179);
+            this.labelbooking.Name = "labelbooking";
+            this.labelbooking.Size = new System.Drawing.Size(249, 96);
+            this.labelbooking.TabIndex = 7;
+            this.labelbooking.Text = "Book an\r\n appointment";
+            this.labelbooking.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelapp
+            // 
+            this.labelapp.AutoSize = true;
+            this.labelapp.BackColor = System.Drawing.Color.Transparent;
+            this.labelapp.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelapp.ForeColor = System.Drawing.Color.White;
+            this.labelapp.Location = new System.Drawing.Point(395, 179);
+            this.labelapp.Name = "labelapp";
+            this.labelapp.Size = new System.Drawing.Size(265, 96);
+            this.labelapp.TabIndex = 8;
+            this.labelapp.Text = "List of\r\n appointments";
+            this.labelapp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labeldentist
+            // 
+            this.labeldentist.AutoSize = true;
+            this.labeldentist.BackColor = System.Drawing.Color.Transparent;
+            this.labeldentist.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labeldentist.ForeColor = System.Drawing.Color.White;
+            this.labeldentist.Location = new System.Drawing.Point(722, 179);
+            this.labeldentist.Name = "labeldentist";
+            this.labeldentist.Size = new System.Drawing.Size(223, 96);
+            this.labeldentist.TabIndex = 9;
+            this.labeldentist.Text = "Dentist\'s\r\nInformation";
+            this.labeldentist.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelprofile
+            // 
+            this.labelprofile.AutoSize = true;
+            this.labelprofile.BackColor = System.Drawing.Color.Transparent;
+            this.labelprofile.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelprofile.ForeColor = System.Drawing.Color.White;
+            this.labelprofile.Location = new System.Drawing.Point(134, 545);
+            this.labelprofile.Name = "labelprofile";
+            this.labelprofile.Size = new System.Drawing.Size(164, 96);
+            this.labelprofile.TabIndex = 10;
+            this.labelprofile.Text = "View my\r\nprofile\r\n";
+            this.labelprofile.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelpharmacy
+            // 
+            this.labelpharmacy.AutoSize = true;
+            this.labelpharmacy.BackColor = System.Drawing.Color.Transparent;
+            this.labelpharmacy.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelpharmacy.ForeColor = System.Drawing.Color.White;
+            this.labelpharmacy.Location = new System.Drawing.Point(434, 569);
+            this.labelpharmacy.Name = "labelpharmacy";
+            this.labelpharmacy.Size = new System.Drawing.Size(185, 48);
+            this.labelpharmacy.TabIndex = 11;
+            this.labelpharmacy.Text = "Pharmacy";
+            this.labelpharmacy.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(746, 545);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 96);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Acquired\r\nmedicines\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Menu";
+            this.BackgroundImage = global::ClinicaPOO.Properties.Resources.HD_wallpaper_plain_purple_background_purple;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1022, 968);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelpharmacy);
+            this.Controls.Add(this.labelprofile);
+            this.Controls.Add(this.labeldentist);
+            this.Controls.Add(this.labelapp);
+            this.Controls.Add(this.labelbooking);
+            this.Controls.Add(this.btn_medicines);
+            this.Controls.Add(this.btn_pharmacy);
+            this.Controls.Add(this.btn_profile);
+            this.Controls.Add(this.btn_dentists);
+            this.Controls.Add(this.btn_bookapp);
+            this.Controls.Add(this.btn_listapp);
+            this.Controls.Add(this.barramenu);
+            this.Name = "Menu";
+            this.Text = "Dentist\'s\r\nInformation";
+            ((System.ComponentModel.ISupportInitialize)(this.barramenu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_listapp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_bookapp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_dentists)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_profile)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_pharmacy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_medicines)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox barramenu;
+        private System.Windows.Forms.PictureBox btn_listapp;
+        private System.Windows.Forms.PictureBox btn_bookapp;
+        private System.Windows.Forms.PictureBox btn_dentists;
+        private System.Windows.Forms.PictureBox btn_profile;
+        private System.Windows.Forms.PictureBox btn_pharmacy;
+        private System.Windows.Forms.PictureBox btn_medicines;
+        private System.Windows.Forms.Label labelbooking;
+        private System.Windows.Forms.Label labelapp;
+        private System.Windows.Forms.Label labeldentist;
+        private System.Windows.Forms.Label labelprofile;
+        private System.Windows.Forms.Label labelpharmacy;
+        private System.Windows.Forms.Label label1;
     }
 }
