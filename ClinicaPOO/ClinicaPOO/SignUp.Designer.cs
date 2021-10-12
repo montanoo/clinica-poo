6d0d517e9f1c7fe7c22f1b83ea5f4282ef79a4c5
Ivania <ivanialebron4.180@gmail,com>
2021-10-11 23:52:23 -0600

    Design Sign Up 👍

---
+++ ClinicaPOO/ClinicaPOO/SignUp.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace ClinicaPOO
+{
+    partial class SignUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SignUp));
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtlastname = new System.Windows.Forms.TextBox();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.txtDUI = new System.Windows.Forms.TextBox();
+            this.txtphone = new System.Windows.Forms.TextBox();
+            this.dTPbirth = new System.Windows.Forms.DateTimePicker();
+            this.btnsignup = new ClinicaPOO.RoundedSignUp();
+            this.backtologin = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.backtologin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtname
+            // 
+            this.txtname.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtname.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtname.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtname.Location = new System.Drawing.Point(134, 399);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(518, 36);
+            this.txtname.TabIndex = 0;
+            // 
+            // txtlastname
+            // 
+            this.txtlastname.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtlastname.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtlastname.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtlastname.Location = new System.Drawing.Point(827, 399);
+            this.txtlastname.Name = "txtlastname";
+            this.txtlastname.Size = new System.Drawing.Size(518, 36);
+            this.txtlastname.TabIndex = 1;
+            // 
+            // txtemail
+            // 
+            this.txtemail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtemail.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtemail.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtemail.Location = new System.Drawing.Point(134, 553);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(518, 36);
+            this.txtemail.TabIndex = 2;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtpassword.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtpassword.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtpassword.Location = new System.Drawing.Point(827, 553);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.PasswordChar = '●';
+            this.txtpassword.Size = new System.Drawing.Size(518, 36);
+            this.txtpassword.TabIndex = 3;
+            // 
+            // txtDUI
+            // 
+            this.txtDUI.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDUI.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtDUI.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtDUI.Location = new System.Drawing.Point(144, 724);
+            this.txtDUI.Name = "txtDUI";
+            this.txtDUI.Size = new System.Drawing.Size(508, 36);
+            this.txtDUI.TabIndex = 4;
+            // 
+            // txtphone
+            // 
+            this.txtphone.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtphone.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtphone.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtphone.Location = new System.Drawing.Point(827, 724);
+            this.txtphone.Name = "txtphone";
+            this.txtphone.Size = new System.Drawing.Size(518, 36);
+            this.txtphone.TabIndex = 5;
+            // 
+            // dTPbirth
+            // 
+            this.dTPbirth.AccessibleRole = System.Windows.Forms.AccessibleRole.ScrollBar;
+            this.dTPbirth.CalendarFont = new System.Drawing.Font("MS Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dTPbirth.CalendarMonthBackground = System.Drawing.Color.PaleTurquoise;
+            this.dTPbirth.CalendarTitleBackColor = System.Drawing.Color.DarkViolet;
+            this.dTPbirth.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dTPbirth.Location = new System.Drawing.Point(134, 891);
+            this.dTPbirth.MaxDate = new System.DateTime(2005, 1, 1, 0, 0, 0, 0);
+            this.dTPbirth.MinDate = new System.DateTime(1900, 1, 1, 0, 0, 0, 0);
+            this.dTPbirth.Name = "dTPbirth";
+            this.dTPbirth.Size = new System.Drawing.Size(518, 43);
+            this.dTPbirth.TabIndex = 6;
+            this.dTPbirth.Value = new System.DateTime(2005, 1, 1, 0, 0, 0, 0);
+            // 
+            // btnsignup
+            // 
+            this.btnsignup.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(32)))), ((int)(((byte)(125)))));
+            this.btnsignup.Font = new System.Drawing.Font("Myanmar Text", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnsignup.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnsignup.Location = new System.Drawing.Point(947, 835);
+            this.btnsignup.Name = "btnsignup";
+            this.btnsignup.Size = new System.Drawing.Size(240, 99);
+            this.btnsignup.TabIndex = 7;
+            this.btnsignup.Text = "Sign Up";
+            this.btnsignup.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnsignup.UseVisualStyleBackColor = false;
+            // 
+            // backtologin
+            // 
+            this.backtologin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(6)))), ((int)(((byte)(106)))));
+            this.backtologin.Image = ((System.Drawing.Image)(resources.GetObject("backtologin.Image")));
+            this.backtologin.Location = new System.Drawing.Point(1357, 12);
+            this.backtologin.Name = "backtologin";
+            this.backtologin.Size = new System.Drawing.Size(53, 53);
+            this.backtologin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.backtologin.TabIndex = 8;
+            this.backtologin.TabStop = false;
+            this.backtologin.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // SignUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1422, 977);
+            this.Controls.Add(this.backtologin);
+            this.Controls.Add(this.btnsignup);
+            this.Controls.Add(this.dTPbirth);
+            this.Controls.Add(this.txtphone);
+            this.Controls.Add(this.txtDUI);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.txtlastname);
+            this.Controls.Add(this.txtname);
+            this.Name = "SignUp";
+            this.Text = "SignUp";
+            ((System.ComponentModel.ISupportInitialize)(this.backtologin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtlastname;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.TextBox txtDUI;
+        private System.Windows.Forms.TextBox txtphone;
+        private System.Windows.Forms.DateTimePicker dTPbirth;
+        private RoundedSignUp btnsignup;
+        private System.Windows.Forms.PictureBox backtologin;
+    }
+}
