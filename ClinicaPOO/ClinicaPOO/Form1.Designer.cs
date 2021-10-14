05aa48e0f5cd5bb73e852b2dbe078fab7426faf1
Kallahan Salas <77999591+Kallahans21@users.noreply.github.com>
2021-10-14 17:23:59 -0600

    appointment creation view

--- ClinicaPOO/ClinicaPOO/Form1.Designer.cs
+++ ClinicaPOO/ClinicaPOO/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace ClinicaPOO
 {
-    partial class Form1
+    partial class AppointmentBooking
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,13 +29,317 @@ namespace ClinicaPOO
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AppointmentBooking));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Reason = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dentistcmb = new System.Windows.Forms.ComboBox();
+            this.methodcmb = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.price = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.hourcmbx = new System.Windows.Forms.ComboBox();
+            this.backbtn = new System.Windows.Forms.Button();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(64, 121);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 87);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(401, 180);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 65);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "REASON";
+            // 
+            // Reason
+            // 
+            this.Reason.BackColor = System.Drawing.Color.White;
+            this.Reason.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Reason.Font = new System.Drawing.Font("Fjalla One", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Reason.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.Reason.Location = new System.Drawing.Point(230, 265);
+            this.Reason.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Reason.Name = "Reason";
+            this.Reason.Size = new System.Drawing.Size(556, 56);
+            this.Reason.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(400, 341);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(216, 65);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "DENTIST";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(438, 685);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 65);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "DATE";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(394, 510);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(228, 65);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "METHOD";
+            // 
+            // dentistcmb
+            // 
+            this.dentistcmb.BackColor = System.Drawing.Color.White;
+            this.dentistcmb.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.dentistcmb.Font = new System.Drawing.Font("Fjalla One", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dentistcmb.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.dentistcmb.FormattingEnabled = true;
+            this.dentistcmb.Location = new System.Drawing.Point(231, 427);
+            this.dentistcmb.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.dentistcmb.Name = "dentistcmb";
+            this.dentistcmb.Size = new System.Drawing.Size(555, 58);
+            this.dentistcmb.TabIndex = 6;
+            // 
+            // methodcmb
+            // 
+            this.methodcmb.BackColor = System.Drawing.Color.White;
+            this.methodcmb.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.methodcmb.Font = new System.Drawing.Font("Fjalla One", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.methodcmb.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.methodcmb.FormattingEnabled = true;
+            this.methodcmb.Location = new System.Drawing.Point(231, 596);
+            this.methodcmb.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.methodcmb.Name = "methodcmb";
+            this.methodcmb.Size = new System.Drawing.Size(555, 58);
+            this.methodcmb.TabIndex = 7;
+            this.methodcmb.SelectedIndexChanged += new System.EventHandler(this.methodcmb_SelectedIndexChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Forte", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker1.Font = new System.Drawing.Font("Segoe UI", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(231, 765);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(555, 66);
+            this.dateTimePicker1.TabIndex = 8;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(898, 289);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(155, 65);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "PRICE";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(845, 357);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(261, 82);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // price
+            // 
+            this.price.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.price.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.price.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.price.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.price.Location = new System.Drawing.Point(918, 372);
+            this.price.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.price.Name = "price";
+            this.price.Size = new System.Drawing.Size(115, 54);
+            this.price.TabIndex = 11;
+            this.price.Text = "$0.00";
+            this.price.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.price.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Location = new System.Drawing.Point(894, 598);
+            this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(163, 65);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "HOUR";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(845, 666);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(261, 82);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            // 
+            // hourcmbx
+            // 
+            this.hourcmbx.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.hourcmbx.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.hourcmbx.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.hourcmbx.FormattingEnabled = true;
+            this.hourcmbx.Items.AddRange(new object[] {
+            "12:00",
+            "13:30",
+            "15:00",
+            "16:30",
+            "18:00"});
+            this.hourcmbx.Location = new System.Drawing.Point(866, 678);
+            this.hourcmbx.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.hourcmbx.Name = "hourcmbx";
+            this.hourcmbx.Size = new System.Drawing.Size(218, 62);
+            this.hourcmbx.TabIndex = 14;
+            this.hourcmbx.Text = "00:00";
+            // 
+            // backbtn
+            // 
+            this.backbtn.BackColor = System.Drawing.Color.Transparent;
+            this.backbtn.FlatAppearance.BorderSize = 0;
+            this.backbtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.backbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.backbtn.Image = ((System.Drawing.Image)(resources.GetObject("backbtn.Image")));
+            this.backbtn.Location = new System.Drawing.Point(1082, 688);
+            this.backbtn.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.backbtn.Name = "backbtn";
+            this.backbtn.Size = new System.Drawing.Size(333, 238);
+            this.backbtn.TabIndex = 18;
+            this.backbtn.UseVisualStyleBackColor = false;
+            this.backbtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(1072, 88);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(301, 297);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 19;
+            this.pictureBox6.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(921, 751);
+            this.label7.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(109, 38);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "(24 hrs)";
+            // 
+            // AppointmentBooking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(27F, 65F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1418, 968);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.hourcmbx);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.price);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.methodcmb);
+            this.Controls.Add(this.dentistcmb);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Reason);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.backbtn);
+            this.Controls.Add(this.pictureBox6);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.Name = "AppointmentBooking";
+            this.Text = "Appointment booking";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Reason;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox dentistcmb;
+        private System.Windows.Forms.ComboBox methodcmb;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox price;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ComboBox hourcmbx;
+        private System.Windows.Forms.Button backbtn;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- ClinicaPOO/ClinicaPOO/Form1.cs
+++ ClinicaPOO/ClinicaPOO/Form1.cs
@@ -10,13 +10,40 @@ using System.Windows.Forms;
 
 namespace ClinicaPOO
 {
-    public partial class Form1 : Form
+    public partial class AppointmentBooking : Form
     {
-        public Form1()
+        public AppointmentBooking()
         {
             //Kallahan Salas
             InitializeComponent();
+            var fechaactual = DateTime.Now;
+            dateTimePicker1.MinDate = fechaactual;
+
         }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void methodcmb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
